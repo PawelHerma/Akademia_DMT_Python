aa66351b1fb9f95d66b895e16940298302577d77
PawelHerma <92121734+PawelHerma@users.noreply.github.com>
2022-11-14 20:59:50 +0100

    Fix Zad5_zepsute

--- Programy Python/Funkcje_Python/Zad5_zepsute.py
+++ Programy Python/Funkcje_Python/Zad5_zepsute.py
@@ -2,10 +2,18 @@ def division(nom,denom):
     if denom == 0:
         print("Nieladnie tak dzielic przez 0")
         exit()
-    if nom / denom == (nom / denom) * 10 - (nom * 10 // denom): #nie działa, nie mam pomysłu na to :/
-        print(nom//denom,".(",nom%denom*denom,")")
-    else:
-        print(nom/denom)
+    help1 = nom // denom
+    help = nom
+    while nom != 0:
+        help = nom * 10 % denom
+        if help % denom == nom % denom:
+            print(help1, "(", nom * 10 // denom ,")")
+            break
+        else:
+            help1 += nom * 10 // denom / 10
+            nom = nom * 10 % denom
+            print(help1)
+
 
 nom = int(input("Podaj licznik: "))
 denom = int(input("Podaj mianownik: "))
